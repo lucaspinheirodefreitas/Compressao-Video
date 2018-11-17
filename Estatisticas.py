f08d7821a46cd0af9f5e1d8977941257e00d806c
Lucas Pinheiro <lucaspinheirodefreitas@gmail.com>
2018-11-17 11:59:57 -0200

    inserção de grafico com comparação entre os metodos de compressão MPEG4 e H264.

--- Estatisticas.py
+++ Estatisticas.py
@@ -1,14 +1,48 @@
 # -*- coding: utf-8 -*-
 
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Estatisticas(object):
-    def __init__(self, tempo, tamanho_inicial, tamanho_final):
-        self.tempo = tempo
+    def __init__(self, tamanho_inicial, numero_videos):
         self.tamanho_inicial = tamanho_inicial
-        self.tamanho_final = tamanho_final
-
+        self.numero_videos = numero_videos
+        self.tamanho_final_h264 = []
+        self.tempo_h264 = []
+        self.tamanho_final_mpeg4 = []
+        self.tempo_mpeg4 = []
+        self.nm_videos = []
+    '''
     def imprime_dados(self):
-        print("\nO tempo gasto para comprimir o video foi: %d " % self.tempo + ("segundos." if (self.tempo > 1) else "segundo."))
-        print("O tamanho inicial do video era: %.3f MBs." % (self.tamanho_inicial/1000000.0))
-        print("O tamanho final do video é: %.3f MBs." % (self.tamanho_final/1000000.0))
+        for i in range(self.numero_videos):
+            print("\nO tempo gasto para comprimir o video foi: %d " % self.tempo[i] + ("segundos." if (self.tempo[i] > 1) else "segundo."))
+            print("O tamanho inicial do video era: %.3f MBs." % (self.tamanho_inicial[i]/1000000.0))
+            print("O tamanho final do video é: %.3f MBs." % (self.tamanho_final[i]/1000000.0))
+    '''
+
+    '''
+    Como utilizar a biblioteca matplotlib:
+        https://www.vooo.pro/insights/guia-basico-para-plotar-graficos-usando-o-matplotlib-do-python/
+        https://www.vooo.pro/insights/guia-basico-para-plotar-graficos-usando-o-matplotlib-do-python-parte-2/
+    '''
+
+    def nome_videos(self):
+        for i in range(self.numero_videos):
+            self.nm_videos.append("video" + str(i+1))
+        return self.nm_videos
+
+    def plota_comparativo(self, tamanho_final_h264, tempo_h264, tamanho_final_mpeg4, tempo_mpeg4):
+        fig, ax = plt.subplots()
+        indice = np.arange(self.numero_videos)
+        bar_larg = 0.4
+        transp = 0.8
+        plt.bar(indice, tamanho_final_mpeg4, bar_larg, alpha=transp, color="orange", label='compressaoMPEG')
+        plt.bar(indice + bar_larg, tamanho_final_h264, bar_larg, alpha=transp, color="gray", label='compressaoH264')
+
+        plt.xlabel('quantidade de videos')
+        plt.ylabel('tamanho do video')
+        plt.title('Compressao H264 x MPEG')
+        plt.xticks(indice + (bar_larg/2), self.nome_videos())
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
--- PathVideos.py
+++ PathVideos.py
@@ -5,13 +5,13 @@ class PathVideos(object):
     def __init__(self):
         self.VIDEO_ENTRADA = 'video'
         self.VIDEO_SAIDA = 'convert-video'
-        self.LOCAL_VIDEOS = '/Videos/'
         self.VIDEOS_ENTRADA = []
         self.VIDEOS_SAIDA = []
+        self.numero_videos = 0
 
     def new_path(self):
-        numero_videos = input("Digite a quantidade de videos que deseja comprimir: ")
-        for i in range(numero_videos):
+        self.numero_videos = input("Digite a quantidade de videos que deseja comprimir: ")
+        for i in range(self.numero_videos):
             self.VIDEOS_ENTRADA.append(self.VIDEO_ENTRADA + str(i+1) + ".mp4")
             self.VIDEOS_SAIDA.append(self.VIDEO_SAIDA + str(i+1))
 
